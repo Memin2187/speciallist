4c4cd9db88fd00e642a9def3765f5e65f776bb40
Francisco del Villar <francisco.sdv.mtz@hotmail.com>
2022-02-12 15:05:22 -0600

    Commit de citas

--- proyecto/frontend/speciallist/src/components/cita/Cita.js
+++ proyecto/frontend/speciallist/src/components/cita/Cita.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export const Cita = () => {
-  return <div>INICIO DE CITAS</div>;
+  return (
+      <div className='container p-160-cont pt-sm-60'>
+
+      {/* <!-- LOGIN / REGISTER CONTAINER --> */}
+        <div className="row">
+          Francisco
+          sple
+          tabla-horario
+          Calendario
+        </div>
+      </div>   
+  );
 };
